0d58ec05e0dde4b5cb62da4f8fed2d8b78cab67e
YujeongLEE <dldbwjd24@ewhain.net>
2024-01-21 12:11:33 +0900

    main activity 분석

--- app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() { //MainActivity -> AppCompatActivity -> ComponentActivity -> Activity
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) { //앱이 특정 상황(메모리 부족으로 강제종료 등)에서 종료될 경우 상황을 저장해서 실행시 돌려줌(default = null)
+        super.onCreate(savedInstanceState) //
+        setContentView(R.layout.activity_lyj)//보여줄 레이아웃을 지정해줌
+        //R : 모든 리소스를 식별할 수 있는 변수가 정의됨 ex)리소스의 색, 리소스의 문자열 등등
+        //R.color 로 해당 리소스의 색에 접근할 수 있다는 의미
     }
 }
